915ed8d24ff8a7f2bba48dbc2589e5a7a7888d08
LeeJunYeoung <soo28819@gmail.com>
2016-12-29 21:52:20 +0900

    push test - NLN

--- app/src/main/java/com/nemo/touchnemo/MainActivity.java
+++ app/src/main/java/com/nemo/touchnemo/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         newProjectBtn = (Button) findViewById(R.id.new_project_btn);
