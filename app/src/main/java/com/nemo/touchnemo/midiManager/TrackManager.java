99d93f88ae68969d24bf095e6bbc026b57459567
LeeJunYeoung <soo28819@gmail.com>
2016-12-29 22:26:34 +0900

    beta 1.0

--- app/src/main/java/com/nemo/touchnemo/midiManager/TrackManager.java
+++ app/src/main/java/com/nemo/touchnemo/midiManager/TrackManager.java
@@ -4,4 +4,20 @@ public class TrackManager
 {
     static final byte[] IDENTIFIER = { 'M', 'T', 'r', 'k' };
 
+    private static TrackManager myInstance;
+
+    public static TrackManager getInstance()
+    {
+        if(myInstance == null)
+        {
+            myInstance = new TrackManager();
+        }
+
+        return myInstance;
+    }
+
+    private TrackManager()
+    {
+        //empty
+    }
 }
