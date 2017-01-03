082f0abb0bac10c8c3bc075ab0823df06691bb6c
LeeJunYeoung <soo28819@gmail.com>
2017-01-03 17:55:12 +0900

    NLN 2017-1-3

--- app/src/main/java/com/nemo/touchnemo/midimanager/MidiManager.java
+++ app/src/main/java/com/nemo/touchnemo/midimanager/MidiManager.java
@@ -2,5 +2,20 @@ package com.nemo.touchnemo.midimanager;
 
 public class MidiManager
 {
+    private static MidiManager myInstance;
 
+    public static MidiManager getInstance()
+    {
+        if(myInstance == null)
+        {
+            myInstance = new MidiManager();
+        }
+
+        return myInstance;
+    }
+
+    private MidiManager()
+    {
+        //empty
+    }
 }
